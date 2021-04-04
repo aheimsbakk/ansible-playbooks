c26d2429422971b654b5ea5abcd6677b416b74ad
aheimsbakk <arnulf.heimsbakk@gmail.com>
2021-04-04 02:04:14 +0200

    cleanup

--- files/departure.sh
+++ files/departure.sh
@@ -4,83 +4,93 @@ set -e
 # Find stations: https://stoppested.entur.org/
 # API IDE: https://api.entur.io/journey-planner/v2/ide/
 
-DATETIME=""
-FIGLET="figlet -w 53 -c -C utf8  -k -f mini"
-FROMTTEXT=""
-NEED_UPDATE=0
-STATION="NSR:StopPlace:5823"
-TMP_FILE="/tmp/departure.tmp"
-DEPARTURES_COUNT=2
+DEP_DATETIME=""
+DEP_FRONTTEXT=""
+DEP_SITUATIONS=""
+FIGLET="figlet -w $(tput cols) -c -C utf8  -k -f mini"
+STATION_COUNT=2
+STATION_ID="NSR:StopPlace:5823"
+UPDATED_JSON_FILE="$(mktemp --suffix -json)"
+UPDATED_TIME_FILE="$(mktemp --suffix -time)"
+UPDATE_FREQ_MIN=5
+
+trap 'rm -f "$UPDATED_JSON_FILE" "$UPDATED_TIME_FILE"' INT TERM QUIT
 
 function get_update() {
   curl -s https://api.entur.io/journey-planner/v2/graphql \
        -H 'Content-Type: application/json' \
        -H 'ET-Client-Name: heimsbakk.no-rasberrypi' \
-       -d '{"query":"{stopPlace(id:\"'$STATION'\"){id name estimatedCalls(timeRange:72100 numberOfDepartures:'$DEPARTURES_COUNT' omitNonBoarding:true){expectedDepartureTime destinationDisplay{frontText}serviceJourney{journeyPattern{line{transportMode}}}situations{description{language value}}}}}"}' > "$TMP_FILE"
+       -d '{"query":"{stopPlace(id:\"'$STATION_ID'\"){id name estimatedCalls(timeRange:72100 numberOfDepartures:'$STATION_COUNT' omitNonBoarding:true){expectedDepartureTime destinationDisplay{frontText}serviceJourney{journeyPattern{line{transportMode}}}situations{description{language value}}}}}"}'
 }
 
 function get_departure_data() {
-  local number=$1
+  local number json
+  number="$1"
+  json="$2"
 
-  DATETIME=$(cat "$TMP_FILE" | jq -r ".data.stopPlace.estimatedCalls[$number] | .expectedDepartureTime")
-  FRONTTEXT=$(cat "$TMP_FILE" | jq -r ".data.stopPlace.estimatedCalls[$number] | .destinationDisplay.frontText")
-  SITUATIONS=$(cat "$TMP_FILE" | jq -r ".data.stopPlace.estimatedCalls[1] | .situations[]?.description[]? |  select(.language == \"no\") | .value")
+  DEP_DATETIME=$(echo "$json" | jq -r ".data.stopPlace.estimatedCalls[$number] | .expectedDepartureTime")
+  DEP_FRONTTEXT=$(echo "$json" | jq -r ".data.stopPlace.estimatedCalls[$number] | .destinationDisplay.frontText")
+  DEP_SITUATIONS=$(echo "$json" | jq -r ".data.stopPlace.estimatedCalls[1] | .situations[]?.description[]? |  select(.language == \"no\") | .value")
 }
 
 function get_datetime_diff() {
-  local datetime="$1"
+  local datetime
+  datetime="$1"
+
   echo $(( $(date -d"$datetime" +%s) - $(date +%s) ))
 }
 
 function get_departure() {
-  local datetime="$1"
-  local fronttext="$2"
-  local sec="$(get_datetime_diff "$datetime")"
+  local datetime fronttext sec
+  datetime="$1"
+  fronttext="$2"
+  sec="$(get_datetime_diff "$datetime")"
 
   [[ -z "$fronttext" ]] || printf "%s\n" "$fronttext"
   printf "%02d:%02d\n" $(( sec / 60 )) $(( sec % 60 ))
 }
 
 function get_overview() {
-  # Delete file if data is older than 15 minutes
-  find "$TMP_FILE" -mmin +15 -delete
+  local updated_json updated_time
+  updated_json="$(cat "$UPDATED_JSON_FILE")"
+  updated_time="$(cat "$UPDATED_TIME_FILE")"
 
-  # Do we have local data, and is it new enough
-  if [[ -f "$TMP_FILE" ]]; then
-    get_departure_data 0
-    time_diff="$(get_datetime_diff "$DATETIME")"
-    if [[ 0 -ge $time_diff ]]; then
-      NEED_UPDATE=1
-    fi
+  # check if we need update
+  if [[ $(( updated_time + (60 * UPDATE_FREQ_MIN) )) -lt $(date +%s) ]]; then
+    updated_json=""
   fi
 
-  # Update if DATETIME is not set by this point or NEED_UPDATE
-  if [[ -z "$DATETIME" ]] || [[ $NEED_UPDATE -gt 0 ]]; then
-    get_update
+  # update if no json in buffer
+  if [[ -z "$updated_json" ]]; then
+    get_update > "$UPDATED_JSON_FILE"
+    date +%s > "$UPDATED_TIME_FILE"
   fi
 
-  for (( i=0; i < $DEPARTURES_COUNT; i++ )); do
-    get_departure_data $i
-    get_departure "$DATETIME" "$FRONTTEXT" | $FIGLET
-    [[ -z "$SITUATIONS" ]] || echo $SITUATIONS | fold -sw $(tput cols)
-    [[ -z "$SITUATIONS" ]] && echo --- | $FIGLET
-  done
+  if [[ -n "$updated_json" ]]; then
+    for (( i=0; i < STATION_COUNT; i++ )); do
+      get_departure_data "$i" "$updated_json"
+      get_departure "$DEP_DATETIME" "$DEP_FRONTTEXT" | $FIGLET
+      [[ -z "$DEP_SITUATIONS" ]] || echo "$DEP_SITUATIONS" | fold -sw "$(tput cols)"
+      [[ -z "$DEP_SITUATIONS" ]] && echo --- | $FIGLET
+    done
+  fi
 }
 
 function run() {
+  local home ed el
   home=$(tput cup 0 0)        # move to row col
   ed=$(tput ed)               # clear to of screen
   el=$(tput el)               # clear to end of line
   printf '%s%s' "$home" "$ed"
-  echo -n $(tput setaf 7)
-  echo -n $(tput bold)
+  echo -n "$(tput setaf 7)"   # set white text
+  echo -n "$(tput bold)"      # set bold text
   while true
   do
+    local rows cols
     rows=$(( $(tput lines) - 1 ))
     cols=$(tput cols)
-    current_row=1
-    echo "$(get_overview)" | head -n $rows | while IFS= read -r line; do
-      printf '%-*.*s%s\n' $cols $cols "$line" "$el"
+    get_overview | head -n $rows | while IFS= read -r line; do
+      printf '%-*.*s%s\n' "$cols" "$cols" "$line" "$el"
     done
     printf '%s%s' "$ed" "$home"
     sleep 1
